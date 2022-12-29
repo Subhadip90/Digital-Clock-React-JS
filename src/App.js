392707cbaff58834e383e8961a5ac9b0f49b61b3
Subhadip Chatterjee <50038188+Subhadip90@users.noreply.github.com>
2022-12-29 06:35:43 +0000

    Digital-Clock in React JS using useState Hooks

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  let time = new Date().toLocaleTimeString()
+  const [ctime, setCtime] = useState(time)
+
+  const UpdateTime = () => {
+    time = new Date().toLocaleTimeString()
+    setCtime(time)
+  }
+
+  setInterval(UpdateTime, 1000)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+    <>
+      <h1>{ctime}</h1>
+    </>
+  )
 }
 
-export default App;
+export default App
+
